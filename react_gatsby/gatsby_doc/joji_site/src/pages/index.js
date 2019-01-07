17265acc26d6de9cc9b2131006e3cf9dcb1d6a11
Joji <noypako@hotmail.com>
2019-01-07 12:40:57 -0800

    Initial commit for gatsby js

---
+++ react_gatsby/gatsby_doc/joji_site/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => <div style={{color: 'blue'}}>
+                      <h1>Hello World</h1>
+                      <p>This is a paragraph</p>
+                      <Link to="/page-2/">Page 2</Link>
+                     </div>
---
+++ react_gatsby/gatsby_doc/joji_site/src/pages/page-2.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => <div style={{ color: 'blue' }}>
+                      <h1>This is Page 2</h1>
+                      <Link to="/">Home</Link>
+                    </div>
